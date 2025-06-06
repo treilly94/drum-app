f0749c3a87f88d57ffda1d1c00ccde5adaf7f6f5
Tom Reilly <treilly94@live.com>
2025-06-06 18:43:29 +0100

    feat: Vue Init

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Notation</h1>
+    <client-only placeholder="loading...">
+      <Music />
+    </client-only>
+  </div>
+</template>
---
+++ components/Music.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { Factory } from 'vexflow';
+
+export default {
+    mounted() {
+        const vf = new Factory({
+            renderer: { elementId: 'output', width: 500, height: 200 },
+        });
+
+        const score = vf.EasyScore();
+        const system = vf.System();
+
+        system
+            .addStave({
+                voices: [
+                    score.voice(score.notes('C#5/q, B4, A4, G#4', { stem: 'up' })),
+                    score.voice(score.notes('C#4/h, C#4', { stem: 'down' })),
+                ],
+            })
+            .addClef('percussion')
+        vf.draw();
+    }
+}
+
+</script>
+
+<template>
+    <div id="output"></div>
+</template>
